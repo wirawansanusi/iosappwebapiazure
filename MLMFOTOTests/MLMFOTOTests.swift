898131225ebaeb4501b319a58655abdd0e65a8c4
wirawan <kucingsunter@gmail.com>
2015-09-11 15:54:22 +0700

    Initial Commit

---
+++ MLMFOTOTests/MLMFOTOTests.swift
@@ -0,0 +1,36 @@
+//
+//  MLMFOTOTests.swift
+//  MLMFOTOTests
+//
+//  Created by wirawan sanusi on 9/11/15.
+//  Copyright © 2015 wirawan sanusi. All rights reserved.
+//
+
+import XCTest
+@testable import MLMFOTO
+
+class MLMFOTOTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
